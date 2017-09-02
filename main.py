7babdb8b427e6e5aa8690bf5375bbe4ace48dbe9
Pushkar Anand <anandpushkar088@gmail.com>
2017-09-02 14:38:29 +0530

    Added readme file

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 
-app_id = "FOLDER_ENCR"
+app_id = "FOLDER_ENCRYPTER"
+app_icon = ""
+cat = appindicator.IndicatorCategory.APPLICATION_STATUS
 
 if __name__ == "__main__":
-    ind = appindicator.Indicator.new()
+    ind = appindicator.Indicator.new(app_id, app_icon, cat)
+    ind.set_status(appindicator.IndicatorStatus.ACTIVE)
+    
