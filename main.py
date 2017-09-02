dd09aebfa539a2d35f56967582261e58c2406626
Pushkar Anand <anandpushkar088@gmail.com>
2017-09-02 15:19:53 +0530

    completed writing app indicator icon

--- main.py
+++ main.py
@@ -5,10 +5,30 @@ from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 
 app_id = "FOLDER_ENCRYPTER"
-app_icon = ""
+app_icon = "emblem-nowrite"
 cat = appindicator.IndicatorCategory.APPLICATION_STATUS
+
+def build_menu():
+    menu = gtk.Menu()
+
+    item_start = gtk.MenuItem('Decrypt')
+    #item_start.connect('activate', start)
+    menu.append(item_start)
+
+    item_stop = gtk.MenuItem('Add Folders')
+    #item_stop.connect('activate', stop)
+    menu.append(item_stop)
+
+    item_quit = gtk.MenuItem('Remove Folders')
+    #item_quit.connect('activate', quit)
+    menu.append(item_quit)
+
+    menu.show_all()
+    return menu
+
 
 if __name__ == "__main__":
     ind = appindicator.Indicator.new(app_id, app_icon, cat)
     ind.set_status(appindicator.IndicatorStatus.ACTIVE)
-    
+    ind.set_menu(build_menu())
+    gtk.main()
